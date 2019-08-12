6ee2a88edf256874c86dd9a201ce034bcfc42423
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-12 11:37:58 +0200

    Random 2.0

--- lib/team_detail.dart
+++ lib/team_detail.dart
@@ -5,7 +5,7 @@ class TeamDetail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("List!"),
+        title: Text("Teams"),
         backgroundColor: const Color(0xff000080),
       ),
       body: Center(
@@ -13,9 +13,9 @@ class TeamDetail extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _section("Cristiano Ronaldo", Color(0xffff9933)),
-            _section("Lionel Messi", Color(0xffffffff)),
-            _section("Neymar Jr", Color(0xff138808)),
+            _section("Juventus", Color(0xffff9933)),
+            _section("Manchester City", Color(0xffffffff)),
+            _section("Liverpool", Color(0xff138808)),
           ],
         ),
       ),
