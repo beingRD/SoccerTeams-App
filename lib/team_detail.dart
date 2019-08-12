777d3ec2c7d340fb6b0271f256d93a46c4d7c01f
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-12 12:19:25 +0200

    mock models 1.0

--- lib/team_detail.dart
+++ lib/team_detail.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'models/teams.dart';
 
 class TeamDetail extends StatelessWidget {
+  final Teams teams;
+
+  TeamDetail(this.teams);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -12,22 +17,26 @@ class TeamDetail extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _section("Juventus", Color(0xffff9933)),
-            _section("Manchester City", Color(0xffffffff)),
-            _section("Liverpool", Color(0xff138808)),
-          ],
+          children: _renderFacts(teams),
         ),
       ),
     );
   }
 
-  Widget _section(String title, Color color) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-      ),
-      child: Text(title),
-    );
+  Widget _sectionTitle(String title) {
+    return Text(title);
+  }
+
+  Widget _sectionText(String text) {
+    return Text(text);
+  }
+
+  List<Widget> _renderFacts(Teams teams) {
+    var result = List<Widget>();
+    for (int i = 0; i < teams.facts.length; i++) {
+      result.add(_sectionTitle(teams.facts[i].title));
+      result.add(_sectionText(teams.facts[i].text));
+    }
+    return result;
   }
 }
