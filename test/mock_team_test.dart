7c9f34553d83ba145b7c6dbac82a9130dd7ce4db
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-12 16:37:22 +0200

    code test/mock_team

---
+++ test/mock_team_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:randomapp/mocks/mock_teams.dart';
+
+void main() {
+  test('test fetchAny', () {
+    final mockTeams = MockTeams.fetchAny();
+    expect(mockTeams, isNotNull);
+    expect(mockTeams.name, isNotEmpty);
+  });
+
+  test('test fetchAll', () {
+    final mockTeams = MockTeams.fetchAll();
+    expect(mockTeams.length, greaterThan(0));
+    expect(mockTeams[0].name, isNotEmpty);
+  });
+
+  test('test fetch', () {
+    final mockTeams = MockTeams.fetch(0);
+    expect(mockTeams, isNotNull);
+    expect(mockTeams, isNotEmpty);
+  });
+}
