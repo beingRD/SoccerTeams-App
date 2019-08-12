3132a41b90f7ce0f6be1782c3a6d18b4f0d1d2c9
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-12 11:51:57 +0200

    models

---
+++ lib/models/team_facts.dart
@@ -0,0 +1,5 @@
+class TeamFacts {
+  final String title;
+  final String text;
+  TeamFacts({this.title, this.text});
+}
---
+++ lib/models/teams.dart
@@ -0,0 +1,9 @@
+import 'package:randomapp/models/team_facts.dart';
+
+class Teams {
+  final String name;
+  final String url;
+  final List<TeamFacts> teams;
+  Teams({this.name, this.url, this.teams});
+}
+
