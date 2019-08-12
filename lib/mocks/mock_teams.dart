18d4c56461d57b63f1f284eed6344d1faed6ad4b
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-12 13:23:13 +0200

    url update working

--- lib/mocks/mock_teams.dart
+++ lib/mocks/mock_teams.dart
@@ -3,15 +3,19 @@ import 'package:randomapp/models/teams.dart';
 
 class MockTeams extends Teams {
   static Teams FetchAny() {
-    return Teams(name: 'Juventus', url: '', facts: <TeamFacts>[
-      TeamFacts(
-        title: 'History',
-        text: '...',
-      ),
-      TeamFacts(
-        title: 'Key Players',
-        text: '...',
-      ),
-    ]);
+    return Teams(
+        name: 'Juventus',
+        url:
+            'https://www.juvefc.com/wp-content/uploads/2019/08/FBL-ICC-2019-JUVENTUS-MADRID-1565462820.jpg',
+        facts: <TeamFacts>[
+          TeamFacts(
+            title: 'History',
+            text: '...',
+          ),
+          TeamFacts(
+            title: 'Key Players',
+            text: '...',
+          ),
+        ]);
   }
 }
--- lib/team_detail.dart
+++ lib/team_detail.dart
@@ -10,28 +10,50 @@ class TeamDetail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Teams"),
+        title: Text(teams.name),
         backgroundColor: const Color(0xff000080),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderFacts(teams),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _renderBody(context, teams),
       ),
     );
   }
 
   Widget _sectionTitle(String title) {
-    return Text(title);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+      child: Text(
+        title,
+        textAlign: TextAlign.left,
+        style: TextStyle(color: Colors.black87, fontSize: 25.0),
+      ),
+    );
   }
 
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+      child: Text(text),
+    );
   }
 
-  List<Widget> _renderFacts(Teams teams) {
+  Widget _bannerImage(String url, double height) {
+    return Container(
+      constraints: BoxConstraints.tightFor(height: height),
+      child: Image.network(url, fit: BoxFit.fitWidth),
+    );
+  }
+
+  List<Widget> _renderBody(BuildContext context, Teams team) {
+    var result = List<Widget>();
+    result.add(_bannerImage(teams.url, 170.0));
+    result.addAll(_renderFacts(context, teams));
+    return result;
+  }
+
+  List<Widget> _renderFacts(BuildContext context, Teams team) {
     var result = List<Widget>();
     for (int i = 0; i < teams.facts.length; i++) {
       result.add(_sectionTitle(teams.facts[i].title));
