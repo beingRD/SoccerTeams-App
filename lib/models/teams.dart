9f12a6874533b4ee25217623002b1ad3d7433398
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-13 13:08:49 +0200

    code/json encode

--- lib/models/teams.dart
+++ lib/models/teams.dart
@@ -1,9 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:randomapp/models/team_facts.dart';
 
+part 'teams.g.dart';
+
+@JsonSerializable()
 class Teams {
   final String name;
   final String url;
   final List<TeamFacts> facts;
   Teams({this.name, this.url, this.facts});
-}
 
+  factory Teams.fromJson(Map<String, dynamic> json) => _$TeamsFromJson(json);
+}
