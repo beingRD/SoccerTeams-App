626d4995fcd89a4d093e9c323b1c3176cbe72dd9
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-13 11:57:38 +0200

    code/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:randomapp/mocks/mock_teams.dart';
-import 'package:randomapp/teams_list.dart';
-import 'teams_list.dart';
+import 'package:randomapp/app.dart';
 
 void main() {
-  final mockteams = MockTeams.fetchAll();
-
-  return runApp(MaterialApp(home: TeamsList(mockteams)));
+  return runApp(App());
 }
