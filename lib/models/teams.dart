1f56c6cc3e12743fa8a83bb8b56d66f2597145ec
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-20 17:35:10 +0200

    code/uri http

--- lib/models/teams.dart
+++ lib/models/teams.dart
@@ -1,14 +1,45 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:randomapp/endpoint.dart';
 import 'package:randomapp/models/team_facts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 part 'teams.g.dart';
 
 @JsonSerializable()
 class Teams {
+  final int id;
   final String name;
   final String url;
   final List<TeamFacts> facts;
-  Teams({this.name, this.url, this.facts});
+  Teams({this.id, this.name, this.url, this.facts});
 
   factory Teams.fromJson(Map<String, dynamic> json) => _$TeamsFromJson(json);
+
+  static Future<List<Teams>> fetchAll() async {
+    var uri = Endpoint.uri('/db');
+    final resp = await http.get(uri.toString());
+
+    if (resp.statusCode != 200) throw (resp.body);
+
+    List<Teams> list = List<Teams>();
+    for (var jsonItem in json.decode(resp.body)) {
+      list.add(Teams.fromJson(jsonItem));
+    }
+    return list;
+  }
+
+  static Future<Teams> fetchByID(int id) async {
+    var uri = Endpoint.uri('/db/$id');
+
+    final resp = await http.get(uri.toString());
+
+    if (resp.statusCode != 200) {
+      throw (resp.body);
+    }
+
+    final Map<String, dynamic> itemMap = json.decode(resp.body);
+    return Teams.fromJson(itemMap);
+  }
 }
