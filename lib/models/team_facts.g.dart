d7cb152bff192101893d3d7dd7d4ded07f043adf
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-23 13:29:52 +0200

    generate code

--- lib/models/team_facts.g.dart
+++ lib/models/team_facts.g.dart
@@ -1,9 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'team_facts.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
 TeamFacts _$TeamFactsFromJson(Map<String, dynamic> json) {
   return TeamFacts(
-      title: json['title'] as String, text: json['text'] as String);
+    title: json['title'] as String,
+    text: json['text'] as String,
+  );
 }
 
-Map<String, dynamic> _$TeamFactsToJson(TeamFacts instance) =>
-    <String, dynamic>{'title': instance.title, 'text': instance.text};
+Map<String, dynamic> _$TeamFactsToJson(TeamFacts instance) => <String, dynamic>{
+      'title': instance.title,
+      'text': instance.text,
+    };
--- lib/models/teams.g.dart
+++ lib/models/teams.g.dart
@@ -1,18 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'teams.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
 Teams _$TeamsFromJson(Map<String, dynamic> json) {
   return Teams(
-      name: json['name'] as String,
-      url: json['url'] as String,
-      facts: (json['facts'] as List)
-          ?.map((e) =>
-              e == null ? null : TeamFacts.fromJson(e as Map<String, dynamic>))
-          ?.toList());
+    id: json['id'] as int,
+    name: json['name'] as String,
+    url: json['url'] as String,
+    facts: (json['facts'] as List)
+        ?.map((e) =>
+            e == null ? null : TeamFacts.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
 }
 
 Map<String, dynamic> _$TeamsToJson(Teams instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'url': instance.url,
-      'facts': instance.facts
+      'facts': instance.facts,
     };
